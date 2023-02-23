c74f4110de477f790f692f0de0a1e3295b1d4fc4
jamessumma <90570413+jamessumma@users.noreply.github.com>
2023-02-23 22:55:49 +0000

    light styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (<div>
     <h1>Train Tracks and Tangles</h1>
-    <svg class="window" width="800" height="500" viewBox="0 0 800 500">
+    <svg class="window" width="1000" height="500" viewBox="0 0 1000 500">
       <rect x="0" y="0" width="100%" height="100%" fill="#cccccc" />
     </svg>
-    <button>Left Turn</button>
-    <button>Straight</button>
-    <button>Right Turn</button>
+    <div class="buttons">
+      <button>Left Turn</button>
+      <button>Straight</button>
+      <button>Right Turn</button>
+    </div>
   </div>);
 }
 
